17632129409d65d2978fd510f7b8f33ce8b6ac66
Momen Sh <momen.sh00@gmail.com>
2015-08-30 00:46:17 +0300

    updated index.html

--- files/js/init.js
+++ files/js/init.js
@@ -15,5 +15,45 @@
       }
     );
 
+// Skills progress
+var an = 0;
+$('#skills').waypoint(function() {
+    //console.log('skills');
+    $(function() {
+        $('#skills_progress progress').each(function() {
+            if (an < 6) {
+                var max = $(this).val();
+                $(this).val(0).animate({
+                    value: max
+                }, {
+                    duration: 2000,
+                    easing: 'easeOutCirc'
+                });
+                an++;
+            }
+        });
+    });
+});
+
+// remove the loading overlay
+$('.loading').remove();
+
+// Go to top
+/*Add class when scroll down*/
+$(window).scroll(function(event) {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+        $(".go-top").addClass("show");
+    } else {
+        $(".go-top").removeClass("show");
+    }
+});
+/*Animation anchor*/
+$('.go-top-top').click(function() {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000);
+});
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
