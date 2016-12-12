eb9973c01fe34bbed78c0203f2a3355253a74805
Momen Sh <MomenSh0@users.noreply.github.com>
2016-12-12 20:40:13 +0200

    Update scripts.js

--- test/threejs/js/scripts.js
+++ test/threejs/js/scripts.js
@@ -17,9 +17,27 @@ $(document).ready( function() {
             $(".scores").show();
             $(".bottomInstructions").show();
             $("#gameStartUI").fadeOut("slow",function(){
-//                d = parseInt($('input[name=difficulty]:checked').val());
-//                localStorage.setItem("d", d);
+                d = parseInt($('input[name=difficulty]:checked').val());                
+                difficulty = d;
+                switch (difficulty) {
+                    case 0:
+                        ambientLight.intensity = 0.8;
+                        stoneZIncreaseMin = 3;
+                        stoneZIncreaseMax = 3;
+                        break;
+                    case 1:
+                        ambientLight.intensity = 0.5;
+                        stoneZIncreaseMin = 3;
+                        stoneZIncreaseMax = 5;
+                        break;
+                    case 2:
+                        ambientLight.intensity = 0.3;
+                        stoneZIncreaseMin = 3;
+                        stoneZIncreaseMax = 7;
+                        break;
+                }
                 gameStarted = true;
+                music.play();
             });
         } else {
             $(".nameWarning").fadeIn();
