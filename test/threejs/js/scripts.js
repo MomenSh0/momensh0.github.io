c947ce263bd99cfe2862fa2efd032667a7bb8b41
Momen Sh <MomenSh0@users.noreply.github.com>
2016-12-17 21:59:52 +0200

    Update scripts.js

--- test/threejs/js/scripts.js
+++ test/threejs/js/scripts.js
@@ -1,7 +1,24 @@
 $(document).ready( function() {
     
+    Pace.on('done', function(){
+        $("#blackScreen").hide();
+    });
+
+    var topTen;
     $.ajax({url: "https://script.google.com/macros/s/AKfycbzPxn0GgPN5gVYi9h8yk6nejt2q3qtu12_C2UtfjmhVh2RjmDXS/exec", success: function(result){
-        console.log(result);
+        topTen = result.topTen;
+        //console.log(topTen)
+        //globalTopScore = 0;
+        elements = "<tr>";
+        for(i=0 ; i<topTen.length ; i++){
+            elements += "<tr><td>"+(i+1)+"</td><td>"+topTen[i]['id']+"</td><td>"+topTen[i]['score']+"</td><td>"+topTen[i]['difficulty']+"</td></tr>";
+            if(topTen[i]['score'] > globalTopScore){
+                globalTopScore = topTen[i]['score'];
+            }
+        }
+        elements += "</tr>";
+        $("#leaderboardBody").append(elements);
+        console.log(globalTopScore);
     }});
     
     
@@ -17,9 +34,22 @@ $(document).ready( function() {
             $("#UI1").fadeIn();
         });
     });
+    //Leaderboard button click event
+    $("#leaderboardBtn").click(function(){
+        $("#UI1").fadeOut(function(){
+            $("#leaderboard").fadeIn();
+        });
+    });
+    //Leaderboard back button click event
+    $("#leaderboardBackBtn").click(function(){
+        $("#leaderboard").fadeOut(function(){
+            $("#UI1").fadeIn();
+        });
+    });
     //Game start button event
     $("#gameStartBtn").click(function(){
         if($("#nameInput").val() != ''){
+            playerName = $("#nameInput").val();
             $(".scores").show();
             $(".bottomInstructions").show();
             $(".blaster").show();
@@ -31,16 +61,19 @@ $(document).ready( function() {
                         ambientLight.intensity = 0.8;
                         stoneZIncreaseMin = 3;
                         stoneZIncreaseMax = 3;
+                        difficultyText = "Easy";
                         break;
                     case 1:
                         ambientLight.intensity = 0.5;
                         stoneZIncreaseMin = 3;
                         stoneZIncreaseMax = 5;
+                        difficultyText = "Average";
                         break;
                     case 2:
                         ambientLight.intensity = 0.3;
                         stoneZIncreaseMin = 3;
                         stoneZIncreaseMax = 7;
+                        difficultyText = "Hard";
                         break;
                 }
                 gameStarted = true;
